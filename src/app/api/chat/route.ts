4753b3d8e00e3d0f4e513596b7c76b1fdccf1f17
Jonas Andersen <jonas.y.andersen@traditionasia.com>
2024-12-07 13:42:38 +0700

    add calculator tool

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,11 @@
 import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
+import { tool } from "ai";
+import * as mathjs from "mathjs";
+import { z } from "zod";
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+export const maxDuration = 27; // vercel has a 30 sec limit
+const maxSteps = 10;
 
 export async function POST(req: Request) {
   if (req.headers.get("x-secret") !== process.env.SECRET) {
@@ -12,8 +15,26 @@ export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: openai("gpt-4o-2024-08-06", { structuredOutputs: true }),
+    tools: {
+      calculate: tool({
+        description:
+          "A tool for evaluating mathematical expressions. Example expressions: " +
+          "'1.2 * (2 + 4.5)', '12.7 cm to inch', 'sin(45 deg) ^ 2'.",
+        parameters: z.object({ expression: z.string() }),
+        execute: async ({ expression }) => mathjs.evaluate(expression),
+      }),
+    },
+    maxSteps,
     messages,
+    onStepFinish({ text, toolCalls, toolResults, finishReason, usage }) {
+      console.debug(
+        `Step finished: ${text} (${finishReason})`,
+        toolCalls,
+        toolResults,
+        usage,
+      );
+    },
   });
 
   return result.toDataStreamResponse();
